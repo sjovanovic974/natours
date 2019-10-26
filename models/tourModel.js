2c7a847fc3d7a003d2b244b2eede8e61f0ff25ac
sjovanovic974 <sjovanovic974@gmail.com>
2019-10-26 13:29:12 +0200

    Error Handling Funcionality Added

--- models/tourModel.js
+++ models/tourModel.js
@@ -95,35 +95,16 @@ tourSchema.pre('save', function(next) {
   next();
 });
 
-// tourSchema.pre('save', function(next) {
-//   console.log('Will save document...');
-//   next();
-// });
-
-// tourSchema.post('save', function(doc, next) {
-//   console.log(doc);
-//   next();
-// });
-
 // QUERY MIDDLEWARE
 // tourSchema.pre('find', function(next) {
 tourSchema.pre(/^find/, function(next) {
   this.find({ secretTour: { $ne: true } });
-
-  this.start = Date.now();
-  next();
-});
-
-tourSchema.post(/^find/, function(docs, next) {
-  console.log(`Query took ${Date.now() - this.start} milliseconds!`);
   next();
 });
 
 // AGGREGATION MIDDLEWARE
 tourSchema.pre('aggregate', function(next) {
   this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
-
-  console.log(this.pipeline());
   next();
 });
 
