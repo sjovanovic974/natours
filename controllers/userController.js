920b80abbb4317313fee3ede2d4762b05c3ec1db
sjovanovic974 <sjovanovic974@gmail.com>
2024-07-15 11:18:44 +0200

    Initial commit

---
+++ controllers/userController.js
@@ -0,0 +1,34 @@
+exports.getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route has not been implemented yet!',
+  });
+};
+
+exports.getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route has not been implemented yet!',
+  });
+};
+
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route has not been implemented yet!',
+  });
+};
+
+exports.updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route has not been implemented yet!',
+  });
+};
+
+exports.deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route has not been implemented yet!',
+  });
+};
